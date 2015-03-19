fbffefcbd65e223ca82a25cf472fc2ce0e1ea33e
Patrik Oldsberg <patrik.oldsberg@ericsson.com>
2015-03-19 22:50:22 +0100

    filled in SessionDescription interface and added implementation

--- sdk/src/main/java/com/ericsson/research/owr/sdk/SessionDescription.java
+++ sdk/src/main/java/com/ericsson/research/owr/sdk/SessionDescription.java
@@ -8,4 +8,29 @@ package com.ericsson.research.owr.sdk;
  * An interface for an immutable description of RTC session at different stages.
  */
 public interface SessionDescription {
+    /**
+     * @return the number of media descriptions available
+     */
+    public int getMediaDescriptionCount();
+
+    /**
+     * Returns the media description at index
+     * @param index the index of the media description to get
+     * @return a MediaDescription
+     * @throws java.lang.IndexOutOfBoundsException if index < 0 or index >= count
+     */
+    public StreamDescription getMediaDescriptionByIndex(int index);
+
+    /**
+     * Returns a media description by id, or null if none is found.
+     * Always returns null if null is passed as id.
+     * @param id the id of the media description to get
+     * @return a MediaDescription, or null if none is found
+     */
+    public StreamDescription getMediaDescriptionById(String id);
+
+    /**
+     * @return the id of the session
+     */
+    public long getSessionId();
 }
---
+++ sdk/src/main/java/com/ericsson/research/owr/sdk/SessionDescriptionImpl.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2015 Ericsson Research
+ * Author: Patrik Oldsberg <patrik.oldsberg@ericsson.com>
+ */
+package com.ericsson.research.owr.sdk;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+class SessionDescriptionImpl implements SessionDescription {
+    public static final String TAG = "SessionDescriptionImpl";
+
+    private final ArrayList<StreamDescription> mStreamDescriptions;
+    private final long mSessionId;
+    private Map<String, StreamDescription> mStreamDescriptionMap;
+
+    SessionDescriptionImpl(long sessionId, Collection<StreamDescription> streamDescriptions) {
+        mSessionId = sessionId;
+        mStreamDescriptions = new ArrayList<StreamDescription>(streamDescriptions.size());
+        boolean createdMap = false;
+
+        for (StreamDescription streamDescription : streamDescriptions) {
+            mStreamDescriptions.add(streamDescription);
+            String streamId = streamDescription.getId();
+            if (streamId != null) {
+                if (!createdMap) {
+                    mStreamDescriptionMap = new HashMap<String, StreamDescription>();
+                    createdMap = true;
+                }
+                mStreamDescriptionMap.put(streamId, streamDescription);
+            }
+        }
+    }
+
+    @Override
+    public int getMediaDescriptionCount() {
+        return mStreamDescriptions.size();
+    }
+
+    @Override
+    public StreamDescription getMediaDescriptionByIndex(final int index) {
+        return mStreamDescriptions.get(index);
+    }
+
+    @Override
+    public StreamDescription getMediaDescriptionById(final String id) {
+        if (id == null) {
+            return null;
+        }
+        return mStreamDescriptionMap.get(id);
+    }
+
+    @Override
+    public long getSessionId() {
+        return mSessionId;
+    }
+}
