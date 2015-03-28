ce45941c039712210a453fc11b20dd2005d607bc
Patrik Oldsberg <patrik.oldsberg@ericsson.com>
2015-03-29 00:57:00 +0100

    RtcSessionTest: call test now actually sets up a call an verifies that media sources are received

--- sdk/src/androidTest/java/com/ericsson/research/owr/sdk/RtcSessionTest.java
+++ sdk/src/androidTest/java/com/ericsson/research/owr/sdk/RtcSessionTest.java
@@ -27,19 +27,43 @@ package com.ericsson.research.owr.sdk;
 
 import android.os.Looper;
 import android.test.AndroidTestCase;
+import android.util.Log;
 
+import com.ericsson.research.owr.CaptureSourcesCallback;
 import com.ericsson.research.owr.MediaSource;
 import com.ericsson.research.owr.MediaType;
+import com.ericsson.research.owr.Owr;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.EnumSet;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 
 public class RtcSessionTest extends AndroidTestCase {
     private static final String TAG = "RtcSessionTest";
+
+    static {
+        Owr.init();
+    }
+
+    private MediaSource mVideoSource;
+    private MediaSource mAudioSource;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        Log.v(TAG, "running owr main loop in background");
+        Owr.runInBackground();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        Log.v(TAG, "quitting owr main loop");
+        Owr.quit();
+    }
 
     public void testStuff() {
         RtcConfig config = RtcConfigs.defaultConfig("stun.example.com");
@@ -53,8 +77,6 @@ public class RtcSessionTest extends AndroidTestCase {
     }
 
     public void testCall() {
-        final CountDownLatch lock = new CountDownLatch(1);
-
         RtcConfig config = RtcConfigs.defaultConfig(Collections.<RtcConfig.HelperServer>emptyList());
         final RtcSession out = RtcSessions.create(config);
         final RtcSession in = RtcSessions.create(config);
@@ -63,6 +85,7 @@ public class RtcSessionTest extends AndroidTestCase {
             @Override
             public void onLocalCandidate(final RtcCandidate candidate) {
                 assertSame(Looper.getMainLooper(), Looper.myLooper());
+                Log.e(TAG, "LOCAL CANDIDATE out -> in: " + RtcCandidates.toJsep(candidate));
                 in.addRemoteCandidate(candidate);
             }
         });
@@ -70,63 +93,97 @@ public class RtcSessionTest extends AndroidTestCase {
             @Override
             public void onLocalCandidate(final RtcCandidate candidate) {
                 assertSame(Looper.getMainLooper(), Looper.myLooper());
+                Log.e(TAG, "LOCAL CANDIDATE in -> out: " + RtcCandidates.toJsep(candidate));
                 out.addRemoteCandidate(candidate);
             }
         });
 
-        StreamSetMock streamSetMockOut = new StreamSetMock(Arrays.asList(
+        final StreamSetMock streamSetMockOut = new StreamSetMock("initiator", Arrays.asList(
                 video("video1", true, true),
                 video("video2", true, true),
-                video("video3", true, true),
+                audio("audio1", true, true)
+        ));
+
+        final StreamSetMock streamSetMockIn = new StreamSetMock("peer", Arrays.asList(
+                video("video1", true, true),
                 audio("audio1", true, true),
                 audio("audio2", true, true)
         ));
 
-        final StreamSetMock streamSetMockIn = new StreamSetMock(Arrays.asList(
-                video("video1", true, true),
-                video("video2", true, true),
-                audio("audio1", true, true),
-                audio("audio2", true, true),
-                audio("audio3", true, true)
-        ));
-
-        out.setup(streamSetMockOut, new RtcSession.SetupCompleteCallback() {
+        TestUtils.synchronous().timeout(5).run(new TestUtils.SynchronousBlock() {
             @Override
-            public void onSetupComplete(final SessionDescription localDescription) {
-                assertSame(Looper.getMainLooper(), Looper.myLooper());
-                in.setRemoteDescription(localDescription);
-                in.setup(streamSetMockIn, new RtcSession.SetupCompleteCallback() {
+            public void run(final CountDownLatch latch) {
+                Owr.getCaptureSources(EnumSet.of(MediaType.VIDEO, MediaType.AUDIO), new CaptureSourcesCallback() {
                     @Override
-                    public void onSetupComplete(final SessionDescription localDescription) {
-                        assertSame(Looper.getMainLooper(), Looper.myLooper());
-                        out.setRemoteDescription(localDescription);
-                        lock.countDown();
+                    public void onCaptureSourcesCallback(final List<MediaSource> list) {
+                        Log.d(TAG, "got capture sources: " + list.size());
+                        for (MediaSource source : list) {
+                            Log.d(TAG, "iterate source " + source.getName());
+                            if (source.getMediaType().contains(MediaType.VIDEO) && mVideoSource == null) {
+                                Log.d(TAG, "got video source: " + source + " " + source.getName());
+                                mVideoSource = source;
+                            } else if (source.getMediaType().contains(MediaType.AUDIO) && mAudioSource == null) {
+                                Log.d(TAG, "got audio source: " + source);
+                                mAudioSource = source;
+                            }
+                        }
+                        latch.countDown();
                     }
                 });
             }
         });
 
-        try {
-            lock.await(10, TimeUnit.SECONDS);
-        } catch (InterruptedException e) {
-            throw new RuntimeException("session setup timed out", e);
-        }
+        streamSetMockOut.getStream("audio1").setMediaSource(mAudioSource);
+        streamSetMockIn.getStream("audio1").setMediaSource(mAudioSource);
+        streamSetMockOut.getStream("video1").setMediaSource(mVideoSource);
+        streamSetMockIn.getStream("video1").setMediaSource(mVideoSource);
 
-/*
-//        Java 8
-
-        out.setOnLocalCandidateListener(in::addRemoteCandidate);
-        in.setOnLocalCandidateListener(out::addRemoteCandidate);
-
-        out.setup(simpleStreamSetOut, remoteDesc -> {
-            in.setRemoteDescription(remoteDesc);
-            in.setup(simpleStreamSetIn, out::setRemoteDescription);
+        TestUtils.synchronous().timeout(30).run(new TestUtils.SynchronousBlock() {
+            @Override
+            public void run(final CountDownLatch latch) {
+                out.setup(streamSetMockOut, new RtcSession.SetupCompleteCallback() {
+                    @Override
+                    public void onSetupComplete(final SessionDescription localDescription) {
+                        assertSame(Looper.getMainLooper(), Looper.myLooper());
+                        Log.w(TAG, "OFFER: " + SessionDescriptions.toJsep(localDescription));
+                        in.setRemoteDescription(localDescription);
+                        in.setup(streamSetMockIn, new RtcSession.SetupCompleteCallback() {
+                            @Override
+                            public void onSetupComplete(final SessionDescription localDescription) {
+                                assertSame(Looper.getMainLooper(), Looper.myLooper());
+                                out.setRemoteDescription(localDescription);
+                                Log.w(TAG, "ANSWER: " + SessionDescriptions.toJsep(localDescription));
+                                latch.countDown();
+                            }
+                        });
+                    }
+                });
+                Log.d(TAG, "waiting for call setup");
+            }
         });
-*/
+
+        assertEquals(StreamMode.SEND_RECEIVE, streamSetMockOut.getStream("video1").getStreamMode());
+        assertEquals(StreamMode.SEND_RECEIVE, streamSetMockOut.getStream("audio1").getStreamMode());
+        assertEquals(StreamMode.SEND_RECEIVE, streamSetMockIn.getStream("video1").getStreamMode());
+        assertEquals(StreamMode.SEND_RECEIVE, streamSetMockIn.getStream("audio1").getStreamMode());
+        assertEquals(StreamMode.INACTIVE, streamSetMockOut.getStream("video2").getStreamMode());
+        assertEquals(StreamMode.INACTIVE, streamSetMockIn.getStream("audio2").getStreamMode());
+
+        TestUtils.synchronous().timeout(15).latchCount(4).run(new TestUtils.SynchronousBlock() {
+            @Override
+            public void run(final CountDownLatch latch) {
+                streamSetMockOut.getStream("audio1").waitForRemoteSource(latch);
+                streamSetMockIn.getStream("audio1").waitForRemoteSource(latch);
+                streamSetMockOut.getStream("video1").waitForRemoteSource(latch);
+                streamSetMockIn.getStream("video1").waitForRemoteSource(latch);
+                Log.d(TAG, "waiting for remote sources");
+            }
+        });
+        Log.d(TAG, "got all remote sources");
     }
 
     public void testStreamsetCombinations() {
-        StreamSetMock streamSetMockOut = new StreamSetMock(Arrays.asList(
+        StreamSetMock streamSetMockOut = new StreamSetMock("initiator", Arrays.asList(
                 video("video1", true, true),
                 video("video2", true, true),
                 video("video3", true, true),
@@ -134,7 +191,7 @@ public class RtcSessionTest extends AndroidTestCase {
                 audio("audio2", true, true)
         ));
 
-        final StreamSetMock streamSetMockIn = new StreamSetMock(Arrays.asList(
+        final StreamSetMock streamSetMockIn = new StreamSetMock("peer", Arrays.asList(
                 video("video1", true, true),
                 video("video2", true, true),
                 audio("audio1", true, true),
@@ -145,33 +202,29 @@ public class RtcSessionTest extends AndroidTestCase {
         runStreamSetTest(streamSetMockIn, streamSetMockOut);
     }
 
-    private void runStreamSetTest(final StreamSetMock inbound, StreamSetMock outbound) {
-        final CountDownLatch lock = new CountDownLatch(1);
-
+    private void runStreamSetTest(final StreamSetMock inbound, final StreamSetMock outbound) {
         RtcConfig config = RtcConfigs.defaultConfig(Collections.<RtcConfig.HelperServer>emptyList());
         final RtcSession out = RtcSessions.create(config);
         final RtcSession in = RtcSessions.create(config);
 
-
-        out.setup(outbound, new RtcSession.SetupCompleteCallback() {
+        TestUtils.synchronous().run(new TestUtils.SynchronousBlock() {
             @Override
-            public void onSetupComplete(final SessionDescription localDescription) {
-                in.setRemoteDescription(localDescription);
-                in.setup(inbound, new RtcSession.SetupCompleteCallback() {
+            public void run(final CountDownLatch latch) {
+                out.setup(outbound, new RtcSession.SetupCompleteCallback() {
                     @Override
                     public void onSetupComplete(final SessionDescription localDescription) {
-                        out.setRemoteDescription(localDescription);
-                        lock.countDown();
+                        in.setRemoteDescription(localDescription);
+                        in.setup(inbound, new RtcSession.SetupCompleteCallback() {
+                            @Override
+                            public void onSetupComplete(final SessionDescription localDescription) {
+                                out.setRemoteDescription(localDescription);
+                                latch.countDown();
+                            }
+                        });
                     }
                 });
             }
         });
-
-        try {
-            lock.await(10, TimeUnit.SECONDS);
-        } catch (InterruptedException e) {
-            throw new RuntimeException("session setup timed out", e);
-        }
     }
 
     private static StreamConfig video(String id, boolean wantSend, boolean wantReceive) {
@@ -198,12 +251,24 @@ public class RtcSessionTest extends AndroidTestCase {
 
     private class StreamSetMock extends StreamSet {
         private final ArrayList<Stream> mStreams;
+        private final String mLabel;
 
-        public StreamSetMock(List<StreamConfig> configs) {
+        public StreamSetMock(final String label, List<StreamConfig> configs) {
+            mLabel = label;
             mStreams = new ArrayList<>(configs.size());
             for (StreamConfig config : configs) {
                 mStreams.add(new MediaStreamMock(config));
             }
+        }
+
+        public MediaStreamMock getStream(String id) {
+            for (Stream stream : mStreams) {
+                MediaStreamMock mock = (MediaStreamMock) stream;
+                if (id.equals(mock.getId())) {
+                    return mock;
+                }
+            }
+            return null;
         }
 
         @Override
@@ -212,14 +277,46 @@ public class RtcSessionTest extends AndroidTestCase {
         }
 
         private class MediaStreamMock extends MediaStream {
-            private boolean mGotRemoteSource;
-            private boolean mGotMediaSourceDelegate;
-            private StreamMode mStreamMode;
+            private MediaSource mRemoteSource;
+            private MediaSourceDelegate mMediaSourceDelegate;
+            private StreamMode mStreamMode = null;
+            private MediaSource mLocalSource;
 
             private final StreamConfig mConfig;
+            private CountDownLatch mCountDownLatch = null;
 
             public MediaStreamMock(final StreamConfig config) {
                 mConfig = config;
+            }
+
+            public boolean haveRemoteSource() {
+                return mRemoteSource != null;
+            }
+
+            public boolean haveMediaSourceDelegate() {
+                return mMediaSourceDelegate != null;
+            }
+
+            public MediaSourceDelegate getMediaSourceDelegate() {
+                return mMediaSourceDelegate;
+            }
+
+            public MediaSource getRemoteSource() {
+                return mRemoteSource;
+            }
+
+            public StreamMode getStreamMode() {
+                return mStreamMode;
+            }
+
+            public synchronized void waitForRemoteSource(final CountDownLatch remoteSourceLatch) {
+                if (haveRemoteSource()) {
+                    Log.d(TAG, "[" + mLabel + "] already had remote source for " + getId());
+                    remoteSourceLatch.countDown();
+                    return;
+                }
+                Log.d(TAG, "[" + mLabel + "] waiting for remote source for " + getId());
+                mCountDownLatch = remoteSourceLatch;
             }
 
             @Override
@@ -243,18 +340,33 @@ public class RtcSessionTest extends AndroidTestCase {
             }
 
             @Override
-            void onRemoteMediaSource(final MediaSource mediaSource) {
-                mGotRemoteSource = true;
+            synchronized void onRemoteMediaSource(final MediaSource mediaSource) {
+                Log.v(TAG, "[" + mLabel + "] got remote source for " + getId());
+                mRemoteSource = mediaSource;
+                if (mCountDownLatch != null) {
+                    mCountDownLatch.countDown();
+                }
             }
 
             @Override
             void setMediaSourceDelegate(final MediaSourceDelegate mediaSourceDelegate) {
-                mGotMediaSourceDelegate = true;
+                mMediaSourceDelegate = mediaSourceDelegate;
+                if (mLocalSource != null) {
+                    Log.v(TAG, "[" + mLabel + "] local source set for " + getId() + " : " + mLocalSource);
+                    mediaSourceDelegate.setMediaSource(mLocalSource);
+                } else {
+                    Log.v(TAG, "[" + mLabel + "] local source not set for " + getId() + " : " + mediaSourceDelegate);
+                }
             }
 
             @Override
             public void setStreamMode(final StreamMode mode) {
                 mStreamMode = mode;
+            }
+
+            public void setMediaSource(final MediaSource mediaSource) {
+                Log.v(TAG, "[" + mLabel + "] local source stored for " + getId() + " : " + mediaSource);
+                mLocalSource = mediaSource;
             }
         }
     }
